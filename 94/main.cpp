ef38d2612d52b75bb12438254b54891002053bb1
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-03-19 12:22:16 +0800

    94. Binary Tree Inorder Traversal

---
+++ 94/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "vector"
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> tra;
+
+    vector<int> inorderTraversal(TreeNode *root) {
+        inorder(root);
+        return tra;
+    }
+
+    void inorder(TreeNode *n) {
+        if (n == nullptr) {
+            return;
+        }
+        inorder(n->left);
+        tra.push_back(n->val);
+        inorder(n->right);
+    }
+};
