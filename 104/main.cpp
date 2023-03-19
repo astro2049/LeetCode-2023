991436b8f5c105afe408d8b9aedc67175761c9d4
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-03-19 12:48:02 +0800

    104. Maximum Depth of Binary Tree

---
+++ 104/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "vector"
+#include "queue"
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxD = 0;
+
+    int maxDepth(TreeNode *root) {
+        preorder(root, 0);
+        return maxD;
+    }
+
+    void preorder(TreeNode *n, int depth) {
+        if (n == nullptr) {
+            maxD = max(maxD, depth);
+            return;
+        }
+        depth++;
+        preorder(n->left, depth);
+        preorder(n->right, depth);
+    }
+};
