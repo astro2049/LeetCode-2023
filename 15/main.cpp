aebc9ea14cba09103c62a3dec515b776a741eeb0
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-05-17 16:17:01 +0800

    15. 3Sum

---
+++ 15/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> trips;
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1, k = nums.size() - 1, target = -nums[i];
+            while (j < k) {
+                if (nums[j] + nums[k] > target) {
+                    k--;
+                } else if (nums[j] + nums[k] < target) {
+                    j++;
+                } else {
+                    trips.emplace_back(vector<int>{nums[i], nums[j], nums[k]});
+                    j++;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    k--;
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                }
+            }
+        }
+        return trips;
+    }
+};
