5cd058496449b3e3e8d30ccdbb8d9ea952f235cc
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-05-19 19:59:45 +0800

    21. Merge Two Sorted Lists

--- 21/main.cpp
+++ 21/main.cpp
@@ -19,6 +19,31 @@ struct ListNode {
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+        ListNode *dummy = new ListNode(), *p = dummy;
+        ListNode *l1 = list1, *l2 = list2;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+        if (l1) {
+            p->next = l1;
+        }
+        if (l2) {
+            p->next = l2;
+        }
+        return dummy->next;
+    }
+};
+
+class Solution1 {
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
         ListNode *d = new ListNode(), *j = list1, *k = list2;
         ListNode *r = d;
         while (j && k) {
