b73d0399f9c50dc764a16ed2abd704417f5f83b2
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-05-30 18:00:56 +0800

    746. Min Cost Climbing Stairs

--- 746/main.cpp
+++ 746/main.cpp
@@ -11,6 +11,19 @@ int main() {
 class Solution {
 public:
     int minCostClimbingStairs(vector<int> &cost) {
+        vector<int> dp;
+        dp.push_back(cost[0]);
+        dp.push_back(cost[1]);
+        for (int i = 2; i < cost.size(); i++) {
+            dp.push_back(min(dp[i - 2], dp[i - 1]) + cost[i]);
+        }
+        return min(dp.rbegin()[1], dp.back());
+    }
+};
+
+class Solution1 {
+public:
+    int minCostClimbingStairs(vector<int> &cost) {
         int n = cost.size();
         int pp = cost[0], p = cost[1], c;
         for (int i = 2; i < n; i++) {
