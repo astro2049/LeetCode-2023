612afb921665b9a8f48f6a62155a98a424de08ab
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-06-03 17:27:34 +0800

    1143. Longest Common Subsequence

--- 1143/main.cpp
+++ 1143/main.cpp
@@ -11,6 +11,30 @@ int main() {
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length(), n = text2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        for (int i = 0; i < n + 1; i++) {
+            dp[m][i] = 0;
+        }
+        for (int i = 0; i < m + 1; i++) {
+            dp[i][n] = 0;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (text1[i] == text2[j]) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+class Solution1 {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
         int n_row = text1.length(), n_col = text2.length();
         vector<vector<int>> dp(n_row + 1, vector<int>(n_col + 1));
         for (int i = n_row - 1; i >= 0; i--) {
