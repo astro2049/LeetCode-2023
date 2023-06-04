7d8b0d8593db5466b4e959c167f5f70ecb29053d
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-06-05 00:47:18 +0800

    39. Combination Sum

--- 39/main.cpp
+++ 39/main.cpp
@@ -12,6 +12,35 @@ class Solution {
 public:
     vector<int> candidates;
     int target;
+    vector<vector<int>> res;
+    vector<int> c;
+
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
+        this->candidates = candidates;
+        this->target = target;
+        dfs(0, 0);
+        return res;
+    }
+
+    void dfs(int i, int sum) {
+        if (sum == target) {
+            res.push_back(c);
+            return;
+        } else if (sum > target || i == candidates.size()) {
+            return;
+        } else {
+            c.push_back(candidates[i]);
+            dfs(i, sum + candidates[i]);
+            c.pop_back();
+            dfs(i + 1, sum);
+        }
+    }
+};
+
+class Solution1 {
+public:
+    vector<int> candidates;
+    int target;
     vector<vector<int>> combs;
     vector<int> cur;
 
