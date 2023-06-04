33aa53ac4715d658f6cf45348028ea9012a6b916
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-06-05 01:05:22 +0800

    40. Combination Sum II

--- 40/main.cpp
+++ 40/main.cpp
@@ -14,6 +14,38 @@ public:
     vector<int> candidates;
     int target;
     vector<vector<int>> res;
+    vector<int> c;
+
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        this->candidates = candidates;
+        this->target = target;
+        dfs(0, 0);
+        return res;
+    }
+
+    void dfs(int i, int sum) {
+        if (sum == target) {
+            res.push_back(c);
+            return;
+        } else if (i == candidates.size() || sum > target) {
+            return;
+        } else {
+            c.push_back(candidates[i]);
+            dfs(i + 1, sum + candidates[i]);
+            c.pop_back();
+            for (i++; i < candidates.size() && candidates[i - 1] == candidates[i]; i++) {
+            }
+            dfs(i, sum);
+        }
+    }
+};
+
+class Solution1 {
+public:
+    vector<int> candidates;
+    int target;
+    vector<vector<int>> res;
     vector<int> cur;
 
     vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
