c3d1565ddfb4c6903a237c95cee407fe008a4b31
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-06-06 14:10:22 +0800

    73. Set Matrix Zeroes

--- 73/main.cpp
+++ 73/main.cpp
@@ -12,6 +12,28 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>> &matrix) {
         int m = matrix.size(), n = matrix[0].size();
+        vector<bool> rows(m), cols(n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true, cols[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+class Solution1 {
+public:
+    void setZeroes(vector<vector<int>> &matrix) {
+        int m = matrix.size(), n = matrix[0].size();
         vector<int> row(m);
         vector<int> col(n);
         for (int i = 0; i < m; i++) {
