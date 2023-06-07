874d912c6774b01faf9be217f051e94dbd59304c
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-06-07 19:49:33 +0800

    55. Jump Game

---
+++ 55/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+class Solution {
+public:
+    bool canJump(vector<int> &nums) {
+        for (int i = 0; i < nums.size() - 1;) {
+            if (nums[i] == 0) {
+                return false;
+            } else {
+                if (i + nums[i] >= nums.size() - 1) {
+                    i += nums[i];
+                } else {
+                    int maxJ = -1, maxJump = -1;
+                    for (int j = i + 1; j <= i + nums[i]; j++) {
+                        if (j + nums[j] >= maxJump) {
+                            maxJump = j + nums[j];
+                            maxJ = j;
+                        }
+                    }
+                    i = maxJ;
+                }
+            }
+        }
+        return true;
+    }
+};
