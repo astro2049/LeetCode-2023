6fa871d4e06fa7b05e944d9ef77caeacc53fd07b
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-08-18 18:53:49 +0800

    135. Candy

---
+++ 135/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "vector"
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+class Solution {
+public:
+    int candy(vector<int> &ratings) {
+        vector<int> candies(ratings.size(), 1);
+        for (int i = 1; i < ratings.size(); i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for (int i = ratings.size() - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        int sum = 0;
+        for (const auto &x: candies) {
+            sum += x;
+        }
+        return sum;
+    }
+};
